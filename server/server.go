25f33f90bcd5a3a7942ee9906f66184b9d74a8f9
Seema <seema@Administrators-MacBook-Air-seema.local>
2022-03-30 16:14:22 +0530

    Server and client file

--- server/server.go
+++ server/server.go
@@ -1,1 +1,60 @@
-package
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// if we crash the go code, we get the file name and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	fmt.Println("Connecting to DB")
+
+	connStr := "user=postgres dbname=test1 password=password host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nSuccessfully connected to database!\n")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	s := grpc.NewServer(opts...)
+
+	go func() {
+		fmt.Println("Starting Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	// Wait for Control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// Block until a signal is received
+	<-ch
+
+	// Finally, we stop the server
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("End of Program")
+}
