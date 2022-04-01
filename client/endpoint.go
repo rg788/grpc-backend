0d999050026c966edb25600383d1a3319dc0ed6e
Nikhil <nikil@Air-wifi.local>
2022-04-01 12:30:29 +0530

    Changed endpoint from server to client

---
+++ client/endpoint.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gorilla/mux"
+)
+
+func endPoint() {
+	fmt.Printf("endpoint")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/v1/ports", CreatePort).Methods("POST")
+	r.HandleFunc("/v1/ports/{id}", UpdatePort).Methods("PUT")
+	r.HandleFunc("/v1/ports/{id}", RetreivePort).Methods("GET")
+	r.HandleFunc("/v1/ports/{id}", DeletePort).Methods("DELETE")
+	r.HandleFunc("/v1/ports?page=1&count=10", ListPort).Methods("GET")
+
+}
